e9e64f25721fa2156ee1926f60618fbebe5c1653
ciricstefan <ciric.stefan95@gmail.com>
2019-06-01 22:43:50 +0200

    Create function for toggling class open-active

--- myscript.js
+++ myscript.js
@@ -4,4 +4,10 @@ function toggleOpen() {
     this.classList.toggle("open");
 }
 
+function toggleActive(e) {
+    if(e.propertyName.includes("flex")) {
+        this.classList.toggle("open-active");
+    }
+}
+
 panels.forEach(panel => panel.addEventListener("click", toggleOpen));
